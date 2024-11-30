1d96603013b88848f612cf7fa47be5c749998f21
Suzana Pachelle <suzi.pachelle@gmail.com>
2024-11-30 18:41:56 -0300

    Iniciado lab 2

---
+++ UART_ADC/UART_ADC/Objects/main.d
@@ -0,0 +1,12 @@
+./objects/main.o: main.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\inc\hw_memmap.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\sysctl.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\gpio.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\uart.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\pwm.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\adc.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\pin_map.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\interrupt.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\timer.h
